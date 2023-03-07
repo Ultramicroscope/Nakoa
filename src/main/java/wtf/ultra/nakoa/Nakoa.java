bf28d287296973ecce5a7566826ef7a94fe47863
Ryan <bridgeplayer411@gmail.com>
2023-03-06 22:21:20 -0500

    Reworked Packets & Removed Gui Handling

--- src/main/java/wtf/ultra/nakoa/Nakoa.java
+++ src/main/java/wtf/ultra/nakoa/Nakoa.java
@@ -1,11 +1,16 @@
 package wtf.ultra.nakoa;
 
+import net.minecraft.block.BlockChest;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.inventory.ContainerChest;
 import net.minecraft.network.play.client.C08PacketPlayerBlockPlacement;
+import net.minecraft.network.play.client.C0APacketAnimation;
+import net.minecraft.util.BlockPos;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.MovingObjectPosition;
+import net.minecraft.util.Vec3;
 import net.minecraftforge.client.ClientCommandHandler;
-import net.minecraftforge.client.event.GuiOpenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -19,8 +24,7 @@ public class Nakoa {
 
     private final InitChest nakoa = new InitChest();
     private final Minecraft mc = Minecraft.getMinecraft();
-    private boolean handlingGui = true;
-    private int tick, nakoaState, failCount, stageTwoTick;
+    private int tick, nakoaState, failCount, tickBuffer;
 
     /* Register event handlers and the initialization command. */
     @Mod.EventHandler
@@ -33,52 +37,46 @@ public class Nakoa {
     @SubscribeEvent
     public void onKeyInput(InputEvent.KeyInputEvent event) {
         if (nakoa.isActive() && Keyboard.getEventKey() == Minecraft.getMinecraft().gameSettings.keyBindChat.getKeyCode()) {
-            say("Nakoa has been disabled");
+            say("[NAKOA]: DISABLING");
             end();
-        }
-    }
-
-    /* Prevents Guis to allow Nakoa to run even when Minecraft is minimized or the computer is idle (screensaver). */
-    @SubscribeEvent
-    public void onGuiOpen(GuiOpenEvent event) {
-        if (nakoa.isActive() && !handlingGui) {
-            event.setCanceled(true);
         }
     }
 
     /* Open and close a chest repeatedly using a tick counter to stall actions. */
     @SubscribeEvent
     public void onPlayerTick(TickEvent.PlayerTickEvent event) {
-        // Do nothing until the /nakoa || /nak command has been successfully executed.
-        if (!nakoa.isActive()) return;
+        // Do nothing until the /nakoa command has been successfully executed.
+        if (!nakoa.isActive() || event.phase == TickEvent.Phase.START) return;
+
+        EntityPlayerSP player = mc.thePlayer;
         tick++;
         switch (nakoaState) {
             case 0: {
                 // Prompt user to begin the mod by opening the gold mine chest.
-                say("Open the chest quickly then AFK!");
+                say("[NAKOA]: Open the chest quickly then AFK!");
                 // Progress the stage of the mod.
                 nakoaState = 1;
                 tick = 0;
                 break;
             }
             case 1: {
-                // Wait 160 ticks (8s. 20tps).
-                if (tick == 160) {
+                // Wait some amount of ticks.
+                if (tick == 360) {
                     // Check if the player has opened the chest as they were prompted.
-                    if (mc.thePlayer.openContainer instanceof ContainerChest) {
+                    if (player.openContainer instanceof ContainerChest) {
                         // Close the chest.
-                        mc.thePlayer.closeScreen();
-                        // Set the selected hot-bar slot to slot 2. In Ventureland, slot 2 is never empty.
-                        // When sending a PlayerBlockPlacement packet, it is dropped if the itemStack is null.
-                        mc.thePlayer.inventory.currentItem = 1;
+                        player.closeScreen();
+                        // Set the selected hot-bar slot to slot 2. This ensures consistency
+                        // because in VentureLand, hot-bar slot 2 is always the same.
+                        player.inventory.currentItem = 1;
                         System.out.println("[NAKOA]: STARTING");
                         nakoaState = 2;
                         tick = 0;
-                        // Set a time for the next stage to begin execution. 5 + 0-14 ticks is about 0.25 to 1 second.
-                        stageTwoTick = 5 + (int)(Math.random() * 15);
+                        // Set a time for the next stage to begin execution.
+                        tickBuffer = 5 + (int)(Math.random() * 15);
                     } else {
                         // If the player failed to open the chest in 5 seconds, end execution.
-                        say("No chest was opened");
+                        say("[NAKOA]: No chest was opened");
                         end();
                     }
                 }
@@ -86,57 +84,58 @@ public class Nakoa {
             }
             case 2: {
                 // Once the random number of ticks have passed, begin attempting to reopen the chest.
-                if (tick == stageTwoTick) {
+                if (tick == tickBuffer) {
                     // If this stage fails 4 times, end execution.
                     if (failCount++ == 4) {
-                        say("catastrophic failure");
+                        say("[NAKOA]: CATASTROPHIC FAILURE");
                         end();
                     } else {
-                        // Make sure the player is still within sqrt(10) blocks of the chest.
-                        if (nakoa.chestPos().distanceSq(mc.thePlayer.getPosition()) <= 10) {
-                            // Flag to disable the Gui handler so the chest can be opened and closed.
-                            handlingGui = true;
+                        // Make sure the player is still looking at the chest.
+                        BlockPos blockPos = mc.objectMouseOver.getBlockPos();
+                        if (mc.theWorld.getBlockState(blockPos).getBlock() instanceof BlockChest) {
                             System.out.println("[NAKOA]: OPENING");
-                            // Sending a PlayerBlockPlacement packet to the client's Net Handler queue.
-                            // The face and cursor position parameters are -1 because the server ignores them anyway.
-                            // Technically the player's position and held item are also ignored.
-                            mc.thePlayer.sendQueue.addToSendQueue(new C08PacketPlayerBlockPlacement(mc.thePlayer.getPosition(), -1, mc.thePlayer.getHeldItem(),-1,-1,-1));
+                            
+                            MovingObjectPosition mop = player.rayTrace(6.0, 1.0F);
+                            BlockPos pos = mop.getBlockPos();
+                            int face = mop.sideHit.getIndex();
+                            Vec3 hitVec = mop.hitVec.subtract(pos.getX(), pos.getY(), pos.getZ());
+                            float x = (float)hitVec.xCoord;
+                            float y = (float)hitVec.yCoord;
+                            float z = (float)hitVec.zCoord;
+                            player.sendQueue.addToSendQueue(new C08PacketPlayerBlockPlacement(pos, face, player.inventory.getCurrentItem(), x, y, z));
+                            player.sendQueue.addToSendQueue(new C0APacketAnimation());
+                            
                             nakoaState = 3;
                         } else {
-                            // If the player is too far from the chest, end execution.
+                            // If the player is not looking at the chest, end execution.
                             end();
                         }
                     }
                     // Random number of ticks until the next stage begins.
-                    // Opening a chest needs permission from the server, which takes some time.
-                    // Because of this, immediate progression will cause failure.
-                    // I think if you are too laggy, this time will need to be increased, but I have not seen it fail.
-                    stageTwoTick = 5 + (int)(Math.random() * 15);
+                    tickBuffer = 5 + (int)(Math.random() * 15);
                     tick = 0;
                 }
                 break;
             }
             case 3: {
-                if (tick == stageTwoTick) {
+                if (tick == tickBuffer) {
                     // Check if the chest successfully opened from the packet queued in the previous stage.
-                    if (mc.thePlayer.openContainer instanceof ContainerChest) {
+                    if (player.openContainer instanceof ContainerChest) {
                         System.out.println("[NAKOA]: CLOSING");
                         // Close the chest.
-                        mc.thePlayer.closeScreen();
-                        // Re-enable the Gui handler to prevent Guis from opening.
-                        handlingGui = false;
+                        player.closeScreen();
                         // Reset the fail counter of the previous stage because it was successful.
                         failCount = 0;
-                        // Set the counter to wait 30 to 40 minutes before opening the chest again.
-                        stageTwoTick = 36000 + (int)(Math.random() * 12000);
+                        // Set the counter to wait awhile before opening the chest again.
+                        //tickBuffer = 36000 + (int)(Math.random() * 12000);
+                        tickBuffer = 36 + (int)(Math.random() * 12);
                     } else {
-                        // If the chest failed to open, wait 0.25 to 1 second then try again.
-                        stageTwoTick = 5 + (int)(Math.random() * 15);
+                        // If the chest failed to open, wait a bit then try again.
+                        tickBuffer = 5 + (int)(Math.random() * 15);
                     }
                     nakoaState = 2;
                     tick = 0;
                 }
-                break;
             }
         }
     }
@@ -146,11 +145,11 @@ public class Nakoa {
     }
 
     private void end() {
+        System.out.println("[NAKOA]: DISENGAGING");
         nakoa.deactivate();
-        handlingGui = true;
         tick = 0;
         nakoaState = 0;
         failCount = 0;
-        stageTwoTick = 0;
+        tickBuffer = 0;
     }
 }
